72b2ec79a6a837d6528611ffe8d931196f5dbdc9
Dean Sanders <dmsanders@wisc.edu>
2021-12-05 13:52:46 -0600

    Very close to functional with full hardware. Still some minor issues scaling the ohmic resistance to time, will try Serial.println() to check the outputs

--- coffeeGrindMind.ino
+++ coffeeGrindMind.ino
@@ -1,26 +1,10 @@
-#include <Adafruit_GFX.h>    // Core graphics library
-#include <Adafruit_ST7735.h> // Hardware-specific library
+#include <TFT.h>
 #include <SPI.h>
 
-
-// For the breakout, you can use any 2 or 3 pins
-// These pins will also work for the 1.8" TFT shield
-#define TFT_CS 10
-#define TFT_RST 8  // you can also connect this to the Arduino reset
-                      // in which case, set this #define pin to 0!
-#define TFT_DC 9
-
-
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
-
-// Option 2: use any pins but a little slower!
-#define TFT_SCLK 13   // set these to be whatever pins you like!
-#define TFT_MOSI 11   // set these to be whatever pins you like!
-//Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
-
-// Today, we will look on how to use the 1.8″ ST7735  colored TFT display with Arduino. The past few tutorials have been focused on how to use the Nokia 5110 LCD display extensively
-// but there will be a time when we will need to use a colored display or something bigger with additional features, that’s where the 1.8″ ST7735 TFT display comes in.
-
+// pin definition for the Uno
+#define cs   10
+#define dc   9
+#define rst  8
 
 //LED - 3.3v
 //SCK - D13
@@ -36,6 +20,7 @@ unsigned long mSecDelay = 0;
 int secDelay = 0;   // max size is -32,768 to 32,767 
 int grindStatus = 0;
 int anaVal = 0;
+int timeDelaySec = 0;
 int timeOrWeight = 0;
 int grindType = 0;  // 0 timed grind , 1 weigh based grind
 int weightMapped = 0; // set weight desired by the user in the function
@@ -44,81 +29,61 @@ int currentWeight = 0; //set by a sensor input read by the arduino, likely going
 
 
 //pins
-int analogP1 = 14;
-int analogP2 = 15;
-int grinderActivationPin = 16;
-int grindStatusPin = 17 ;
-int timeOrWeightPin = 18 ;
+int analogP1 = A0;
+int analogP2 = A1;
+int grinderActivationPin = 4;
+int grindStatusPin = 5;
+int timeOrWeightPin = 6;
 
+// create an instance of the library
+TFT TFTscreen = TFT(cs, dc, rst);
+
+// char array to print to the screen
+char GrindTime[2];  // up to 75 sec
+char GrindWeight[3]; // up to 100g
+char seconds[5];
+char grams[5] ;
+int screenWidth = TFTscreen.width();
+int screenHeight = TFTscreen.height();
+int xPosTime = 35 ;
+int yPosTime = screenHeight/3;
+int xPosWeight = 35 ;
+int yPosWeight =  2*(screenHeight/3);
+int x = 0;
+int y = 0;
+
+int r_background = 255;
+int g_background = 255;
+int b_background = 255;
+
+int r_text = 0;
+int g_text = 0;
+int b_text = 0;
 
 
 void setup() {
-  Serial.begin(9600);
-  Serial.print("Hello! ST7735 TFT Test");
-
-  pinMode(grinderActivationPin, OUTPUT);
-  pinMode(grindStatusPin,INPUT);
-  pinMode(analogP1,INPUT);
-
-  // Use this initializer if you're using a 1.8" TFT
-  tft.initR(INITR_BLACKTAB);   // initialize a ST7735S chip, black tab
-
-  // Use this initializer (uncomment) if you're using a 1.44" TFT
-  //tft.initR(INITR_144GREENTAB);   // initialize a ST7735S chip, black tab
-
-  Serial.println("Initialized");
-
-  uint16_t time = millis();
-  tft.fillScreen(ST7735_BLACK);
-  time = millis() - time;
-
-  Serial.println(time, DEC);
-  delay(500);
-
-  // large block of text
-  tft.fillScreen(ST7735_BLACK);
-  testdrawtext("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, fringilla sed malesuada et, malesuada sit amet turpis. Sed porttitor neque ut ante pretium vitae malesuada nunc bibendum. Nullam aliquet ultrices massa eu hendrerit. Ut sed nisi lorem. In vestibulum purus a tortor imperdiet posuere. ", ST7735_WHITE);
-  delay(1000);
-
-  // tft print function!
-  tftPrintTest();
-  delay(4000);
-
-  // a single pixel
-  tft.drawPixel(tft.width()/2, tft.height()/2, ST7735_GREEN);
-  delay(500);
-
-  // line draw test
-  testlines(ST7735_YELLOW);
-  delay(500);
-
-  // optimized lines
-  testfastlines(ST7735_RED, ST7735_BLUE);
-  delay(500);
-
-  testdrawrects(ST7735_GREEN);
-  delay(500);
-
-  testfillrects(ST7735_YELLOW, ST7735_MAGENTA);
-  delay(500);
-
-  tft.fillScreen(ST7735_BLACK);
-  testfillcircles(10, ST7735_BLUE);
-  testdrawcircles(10, ST7735_WHITE);
-  delay(500);
-
-  testroundrects();
-  delay(500);
-
-  testtriangles();
-  delay(500);
-
-  mediabuttons();
-  delay(500);
-
-  Serial.println("done");
-  delay(1000);
+  TFTscreen.begin();
+  // clear the screen with a black background
+  TFTscreen.background(r_background,g_background,b_background);
+  // write the static text to the screen
+  // set the font color to white Called before drawing an object on screen, it sets the color of lines and borders around shapes.
+  TFTscreen.stroke(r_text, g_text, b_text);
+  // set the font size
+  TFTscreen.setTextSize(2);
+  // write the text to the top left corner of the screen
+//  TFTscreen.text("CoffeeGrindMind v0.1\n", screenWidth/8, 5);
+  TFTscreen.text("GrindMindv0.1",5,0);
+  
+  // set the font size very large for the loop
+  TFTscreen.setTextSize(3);
+  // set the color of the bubbles
+  // TFTscreen.fill(255,255,255);
+  char seconds[5] = "sec";
+  TFTscreen.text(seconds,screenWidth/2,screenHeight/3);
+  char grams[5] = "grams";
+  TFTscreen.text(grams,screenWidth/2,2*(screenHeight/3));
 }
+
 
 float mapfloat(long x, long in_min, long in_max, long out_min, long out_max)
 {
@@ -176,215 +141,95 @@ unsigned long checkTimer() {
   return mSecDelay;
 }
 
-void loop() {
-  tft.invertDisplay(true);
-  delay(500);
-  tft.invertDisplay(false);
-  delay(500);
+void writeTimeValue(char GrindTime[2]) {
+  TFTscreen.stroke(r_text, g_text, b_text);
+  TFTscreen.text(GrindTime, xPosTime, yPosTime);
+}
 
+void eraseTimeValue(char GrindTime[2]) {
+  TFTscreen.stroke(r_background,g_background,b_background);
+  TFTscreen.text(GrindTime, xPosTime, yPosTime);
+}
+
+void writeWeightValue(char GrindWeight[3]) {
+  TFTscreen.stroke(r_text, g_text, b_text);
+  TFTscreen.text(GrindWeight, xPosWeight, yPosWeight);
+}
+
+void eraseWeightValue(char GrindWeight[3]) {
+  TFTscreen.stroke(r_background,g_background,b_background);
+  TFTscreen.text(GrindWeight, xPosWeight, yPosWeight);
+}
+
+
+void loop() {
   // *** MAIN GRINDER RUN LOOP ***
   
   // check grind type, timed: 0, weight based: 1
   grindType = checkTimedOrWeightGrind();
   if (grindType == 0){
     timeDelay = checkTimer();
-    Serial.print("Current timer delay is:");
-    Serial.println(timeDelay);
+//    Serial.print("Current timer delay is:");
+//    Serial.println(timeDelay);
+    // convert time delay from up to 99 sec to character array to print to screen.
+    timeDelaySec = timeDelay/1000 ;
+    String tString = String(timeDelaySec);
+    tString.toCharArray(GrindTime,2);
+    writeTimeValue(GrindTime);
     // check if you want immediate grinder activation
     grindStatus = checkGrindActivationStatus();
     if (grindStatus == 1){
       runTimedGrind(timeDelay);
     }
+    // basically display the value then erase it and go look for a change...
+    delay(750);
+    eraseTimeValue(GrindTime);
   } else {
+    //weight based grind
+    setWeight = getSetWeight();
+//    Serial.print("Current weight is selected is: ");
+//    Serial.print(setWeight);
+//    Serial.println(" grams");
+    // convert set weight from up to 999g to character array to print out on screen
+    String wString = String(setWeight);
+    wString.toCharArray(GrindWeight,3);
+    writeWeightValue(GrindWeight);
+    
+    // check if you want immediate grinder activation
+    grindStatus = checkGrindActivationStatus();
     if (grindStatus == 1){
-      setWeight = getSetWeight();
-      Serial.print("Current weight is selected is: ");
-      Serial.print(setWeight);
-      Serial.println(" grams");
-      // check if you want immediate grinder activation
-      grindStatus = checkGrindActivationStatus();
       runWeightBasedGrind(setWeight);
     }
+    // basically display the value then erase it and go look for a change...
+    delay(750);
+    eraseWeightValue(GrindWeight);
   }
 }
 
-void testlines(uint16_t color) {
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawLine(0, 0, x, tft.height()-1, color);
-  }
-  for (int16_t y=0; y < tft.height(); y+=6) {
-    tft.drawLine(0, 0, tft.width()-1, y, color);
-  }
- 
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawLine(tft.width()-1, 0, x, tft.height()-1, color);
-  }
-  for (int16_t y=0; y < tft.height(); y+=6) {
-    tft.drawLine(tft.width()-1, 0, 0, y, color);
-  }
- 
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawLine(0, tft.height()-1, x, 0, color);
-  }
-  for (int16_t y=0; y < tft.height(); y+=6) {
-    tft.drawLine(0, tft.height()-1, tft.width()-1, y, color);
-  }
- 
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawLine(tft.width()-1, tft.height()-1, x, 0, color);
-  }
-  for (int16_t y=0; y < tft.height(); y+=6) {
-    tft.drawLine(tft.width()-1, tft.height()-1, 0, y, color);
-  }
-}
- 
-void testdrawtext(char *text, uint16_t color) {
-  tft.setCursor(0, 0);
-  tft.setTextColor(color);
-  tft.setTextWrap(true);
-  tft.print(text);
-}
- 
-void testfastlines(uint16_t color1, uint16_t color2) {
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t y=0; y < tft.height(); y+=5) {
-    tft.drawFastHLine(0, y, tft.width(), color1);
-  }
-  for (int16_t x=0; x < tft.width(); x+=5) {
-    tft.drawFastVLine(x, 0, tft.height(), color2);
-  }
-}
- 
-void testdrawrects(uint16_t color) {
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color);
-  }
-}
- 
-void testfillrects(uint16_t color1, uint16_t color2) {
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t x=tft.width()-1; x > 6; x-=6) {
-    tft.fillRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color1);
-    tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color2);
-  }
-}
- 
-void testfillcircles(uint8_t radius, uint16_t color) {
-  for (int16_t x=radius; x < tft.width(); x+=radius*2) {
-    for (int16_t y=radius; y < tft.height(); y+=radius*2) {
-      tft.fillCircle(x, y, radius, color);
-    }
-  }
-}
- 
-void testdrawcircles(uint8_t radius, uint16_t color) {
-  for (int16_t x=0; x < tft.width()+radius; x+=radius*2) {
-    for (int16_t y=0; y < tft.height()+radius; y+=radius*2) {
-      tft.drawCircle(x, y, radius, color);
-    }
-  }
-}
- 
-void testtriangles() {
-  tft.fillScreen(ST7735_BLACK);
-  int color = 0xF800;
-  int t;
-  int w = tft.width()/2;
-  int x = tft.height()-1;
-  int y = 0;
-  int z = tft.width();
-  for(t = 0 ; t <= 15; t++) {
-    tft.drawTriangle(w, y, y, x, z, x, color);
-    x-=4;
-    y+=4;
-    z-=4;
-    color+=100;
-  }
-}
- 
-void testroundrects() {
-  tft.fillScreen(ST7735_BLACK);
-  int color = 100;
-  int i;
-  int t;
-  for(t = 0 ; t <= 4; t+=1) {
-    int x = 0;
-    int y = 0;
-    int w = tft.width()-2;
-    int h = tft.height()-2;
-    for(i = 0 ; i <= 16; i+=1) {
-      tft.drawRoundRect(x, y, w, h, 5, color);
-      x+=2;
-      y+=3;
-      w-=4;
-      h-=6;
-      color+=1100;
-    }
-    color+=100;
-  }
-}
- 
-void tftPrintTest() {
-  tft.setTextWrap(false);
-  tft.fillScreen(ST7735_BLACK);
-  tft.setCursor(0, 30);
-  tft.setTextColor(ST7735_RED);
-  tft.setTextSize(1);
-  tft.println("Hello World!");
-  tft.setTextColor(ST7735_YELLOW);
-  tft.setTextSize(2);
-  tft.println("Hello World!");
-  tft.setTextColor(ST7735_GREEN);
-  tft.setTextSize(3);
-  tft.println("Hello World!");
-  tft.setTextColor(ST7735_BLUE);
-  tft.setTextSize(4);
-  tft.print(1234.567);
-  delay(1500);
-  tft.setCursor(0, 0);
-  tft.fillScreen(ST7735_BLACK);
-  tft.setTextColor(ST7735_WHITE);
-  tft.setTextSize(0);
-  tft.println("Hello World!");
-  tft.setTextSize(1);
-  tft.setTextColor(ST7735_GREEN);
-//  tft.print(0, 6);
-  tft.println(" Want pi?");
-  tft.println(" ");
-  tft.print(8675309, HEX); // print 8,675,309 out in HEX!
-  tft.println(" Print HEX!");
-  tft.println(" ");
-  tft.setTextColor(ST7735_WHITE);
-  tft.println("Sketch has been");
-  tft.println("running for: ");
-  tft.setTextColor(ST7735_MAGENTA);
-  tft.print(millis() / 1000);
-  tft.setTextColor(ST7735_WHITE);
-  tft.print(" seconds.");
-}
- 
-void mediabuttons() {
-  // play
-  tft.fillScreen(ST7735_BLACK);
-  tft.fillRoundRect(25, 10, 78, 60, 8, ST7735_WHITE);
-  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST7735_RED);
-  delay(500);
-  // pause
-  tft.fillRoundRect(25, 90, 78, 60, 8, ST7735_WHITE);
-  tft.fillRoundRect(39, 98, 20, 45, 5, ST7735_GREEN);
-  tft.fillRoundRect(69, 98, 20, 45, 5, ST7735_GREEN);
-  delay(500);
-  // play color
-  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST7735_BLUE);
-  delay(50);
-  // pause color
-  tft.fillRoundRect(39, 98, 20, 45, 5, ST7735_RED);
-  tft.fillRoundRect(69, 98, 20, 45, 5, ST7735_RED);
-  // play color
-  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST7735_GREEN);
-}
+//void loop() {
+
+//  // Read the value of the sensor on A0
+//  String sensorVal = String(analogRead(A0));
+
+//  // convert the reading to a char array
+//  sensorVal.toCharArray(sensorPrintout, 4);
+
+//  // set the screen fill color of shapes clear
+//  for (int i = 0; i <= 4; i++) {
+//    TFTscreen.fill(255,255,255);
+//    TFTscreen.stroke(128, 0, 0);
+//    x = random(0,screenWidth);
+//    y = random(60,screenHeight);
+//    // draw a circle in the center of screen
+//    TFTscreen.circle(x,y,5);
+//    // erase the circle
+//    delay(300);
+//    TFTscreen.fill(0,0,0);
+//    TFTscreen.stroke(0,0,0);
+//    TFTscreen.circle(x,y,5);
+//    delay(100);
+//  }
+
+//  // clear the screen with a black background
+//  TFTscreen.background(0,0,0);
+//}
